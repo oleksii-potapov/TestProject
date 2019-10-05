c7a555c1138347ba20220ed6c6ee223c25e2c115
Oleksii Potapov <olexii.potapov@gmail.com>
2019-10-05 09:45:34 +0300

    Add FolderRepository interface

---
+++ FolderProject/Models/IFolderRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderProject.Models
+{
+    public interface IFolderRepository
+    {
+        Folder GetFolderByName(string name);
+
+        IEnumerable<Folder> GetChildFolders(Folder folder);
+
+        Folder AddFolder(string name, Folder parent = null);
+    }
+}
