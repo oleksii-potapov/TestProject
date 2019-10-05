a658c90c6b28ebd41881cbfd0d00c314350be7de
Oleksii Potapov <olexii.potapov@gmail.com>
2019-10-05 10:54:08 +0300

    Add initializer for default data

--- FolderProject/Models/EFFolderRepository.cs
+++ FolderProject/Models/EFFolderRepository.cs
@@ -9,6 +9,27 @@ namespace FolderProject.Models
     {
         private ApplicationContext _context = new ApplicationContext();
 
+        public EFFolderRepository()
+        {
+            if (_context.Folders.Count() == 0)
+                InitializeDb();
+        }
+
+        private void InitializeDb()
+        {
+            Folder root = new Folder { Name = "" };
+            AddFolder(root.Name);
+            Folder folder = new Folder { Name = "Creating Digital Images" };
+            AddFolder(folder.Name, root);
+            Folder folder1 = AddFolder("Resources", folder);
+            AddFolder("Evidence", folder);
+            Folder folder3 = AddFolder("Graphic Products", folder);
+            AddFolder("Primary Sources", folder1);
+            AddFolder("Secondary Sources", folder1);
+            AddFolder("Process", folder3);
+            AddFolder("Final Product", folder3);
+        }
+
         public void AddChild(Folder parent, Folder child)
         {
             parent.ChildFolders.Add(child);
