1497bff763fb327dfdc23b3aa18f77e68a7f89e2
Oleksii Potapov <olexii.potapov@gmail.com>
2019-10-05 09:28:51 +0300

    Add Folder model

---
+++ FolderProject/Models/Folder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace FolderProject.Models
+{
+    public class Folder
+    {
+        [Key]
+        public string Name { get; set; }
+
+        public string ParentPath { get; set; }
+
+        [NotMapped]
+        public string CurrentPath => $"{ParentPath}/{Name}";
+
+        public ICollection<Folder> Childs { get; set; }
+
+        public Folder()
+        {
+            Childs = new List<Folder>();
+        }
+    }
+}
