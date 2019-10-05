62549f59c7ae26d75f71227e9fd383c9b8de6dae
Oleksii Potapov <olexii.potapov@gmail.com>
2019-10-05 09:49:43 +0300

    Add MockRepository for testing functionality

---
+++ FolderProject/Models/MockFolderRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FolderProject.Models
+{
+    public class MockFolderRepository : IFolderRepository
+    {
+        private List<Folder> _folders = new List<Folder>();
+
+        public MockFolderRepository()
+        {
+        }
+
+        public Folder AddFolder(string name, Folder parent = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Folder> GetChildFolders(Folder folder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Folder GetFolderByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
