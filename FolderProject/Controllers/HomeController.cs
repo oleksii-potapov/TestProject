35845ff4ad4f885b8fbd5c7e690c14a2dc7ccd94
Oleksii Potapov <olexii.potapov@gmail.com>
2019-10-05 10:08:32 +0300

    Edit the Home controller for working with the repository interface; Add method for working with catchall parameter

--- FolderProject/Controllers/HomeController.cs
+++ FolderProject/Controllers/HomeController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FolderProject.Models;
 
 namespace FolderProject.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index()
+        public IFolderRepository Repository { get; set; } = new MockFolderRepository();
+
+        public ViewResult Index(string catchall)
         {
-            return View();
+            if (catchall == null)
+                return View(Repository.GetFolderByName(""));
+            else
+                return View(Repository.GetFolderByName(GetNameFromCatchall(catchall)));
+        }
+
+        private string GetNameFromCatchall(string catchall)
+        {
+            catchall = catchall.TrimEnd('/');
+            IEnumerable<string> names = catchall.Split('/');
+            return names.Last();
         }
     }
 }
