624edc253fceb8b201e7fbfba4af10862467092d
Oleksii Potapov <olexii.potapov@gmail.com>
2019-10-06 10:26:50 +0300

    Add autofac config class

---
+++ FolderProject/Utils/AutofacConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using FolderProject.Models;
+
+namespace FolderProject.Utils
+{
+    public class AutofacConfig
+    {
+        public static void ConfigDependencies()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            builder.RegisterType<EFFolderRepository>()
+                .As<IFolderRepository>();
+
+            var container = builder.Build();
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
